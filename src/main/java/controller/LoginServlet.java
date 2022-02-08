41129afe4e8166d5a6bd9da1d97be64e9f737cdc
tungmeo4 <92002319+tungmeo4@users.noreply.github.com>
2022-02-08 17:30:28 +0700

    update
    update

---
+++ src/main/java/controller/LoginServlet.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        action(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        action(request,response);
+    }
+
+    private void action(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null)
+            action = "";
+
+        switch (action) {
+            case "registration":
+                registration(request, response);
+                break;
+            case "login":
+                login(request, response);
+                break;
+        }
+    }
+
+
+}

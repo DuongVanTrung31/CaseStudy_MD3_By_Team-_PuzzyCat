e1001f1dd5259de325dea63e1e0c589bca263822
DuongVanTrung31 <trung.duong.dt31@gmail.com>
2022-02-10 10:53:09 +0700

    fix service Product

--- src/main/java/service/implementService/ProductServiceImplement.java
+++ src/main/java/service/implementService/ProductServiceImplement.java
@@ -9,19 +9,10 @@ import java.util.List;
 
 public class ProductServiceImplement implements IProductService {
     private final IProductDAO productDao = new ProductDAOImplement();
+
     @Override
     public List<Product> getAll() {
         return productDao.getAll();
-    }
-
-    @Override
-    public boolean add(Product product) {
-        return productDao.add(product);
-    }
-
-    @Override
-    public boolean update(int id, Product product) {
-        return productDao.update(id, product);
     }
 
     @Override
@@ -34,4 +25,19 @@ public class ProductServiceImplement implements IProductService {
         return productDao.findById(id);
     }
 
+    @Override
+    public List<Product> findByKeyword(String keyword) {
+        return productDao.findByKeyword(keyword);
+    }
+
+    @Override
+    public boolean add(Product product, int idCategory, int idBrand) {
+        return productDao.add(product,idCategory,idBrand);
+    }
+
+    @Override
+    public boolean update(int id, Product product, int categoryID, int brandID) {
+        return productDao.update(id,product,categoryID,brandID);
+    }
 }
+
--- src/main/java/service/interfaceService/IProductService.java
+++ src/main/java/service/interfaceService/IProductService.java
@@ -3,6 +3,18 @@ package service.interfaceService;
 import model.Product;
 import service.IGeneralService;
 
-public interface IProductService extends IGeneralService<Product> {
+import java.util.List;
 
+public interface IProductService extends IGeneralService<Product> {
+    @Override
+    default boolean update(int id, Product product) {
+        return false;
+    }
+    @Override
+    default boolean add(Product product){
+        return false;
+    };
+    List<Product> findByKeyword(String keyword);
+    boolean add(Product product, int idCategory, int idBrand);
+    boolean update(int id, Product product,int categoryID, int brandID);
 }
